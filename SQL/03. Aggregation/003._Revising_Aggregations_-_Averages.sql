e4f60ec6c69909b67d336a0ec8367ec5952bbb2b
Robaiatul Islam <imkuttumiah@gmail.com>
2018-08-08 21:51:30 +0100

    Solve for Revising Aggregations - Averages.

---
+++ SQL/03. Aggregation/003._Revising_Aggregations_-_Averages.sql
@@ -0,0 +1,9 @@
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem
+-- # Score: 10
+
+
+-- MySQL START
+SELECT AVG(Population)
+FROM CITY
+WHERE District='California';
+-- MySQL END
