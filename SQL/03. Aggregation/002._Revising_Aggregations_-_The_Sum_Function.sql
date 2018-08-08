66d2914088ef6a0522a99890c7d1c0c5a6a049a9
Robaiatul Islam <imkuttumiah@gmail.com>
2018-08-08 21:46:18 +0100

    Solve of Revising Aggregations - The Sum Function.

---
+++ SQL/03. Aggregation/002._Revising_Aggregations_-_The_Sum_Function.sql
@@ -0,0 +1,9 @@
+-- # Problem: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+-- # Score: 10
+
+
+-- MySQL START
+SELECT SUM(Population)
+FROM CITY
+WHERE District='California';
+-- MySQL END
